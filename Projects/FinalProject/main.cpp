9ed24938b7464b81ac80a087bb916859734b7551
Manuell91 <34925152+Manuell91@users.noreply.github.com>
2021-06-03 11:37:06 +0300

    Add files via upload

--- Projects/FinalProject/main.cpp
+++ Projects/FinalProject/main.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+#include <curses.h>
+using namespace std;
+
+int main()
+{
+    initscr(); /* Start curses mode */
+    printw("Hello World !!!"); /* Print Hello World */
+    refresh(); /* Print it on to the real screen */
+    getch(); /* Wait for user input */
+    endwin(); /* End curses mode */
+    return 0;
+}
 
